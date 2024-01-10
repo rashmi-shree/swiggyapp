3c1872f61808953bf600ef854dac5c0a2972ee2e
rash010 <rashmi@sensarobotics.com>
2024-01-10 14:57:11 +0530

    swiggy map

--- src/components/Body.js
+++ src/components/Body.js
@@ -37,7 +37,7 @@ const Body = () => {
       </div>
       <div className='flex flex-wrap'>
         {
-            filteredResList.map((restaurant)=>
+            filteredResList?.map((restaurant)=>
             (
                 <Link
                     to={"/restaurantmenu/"+restaurant?.info?.id}
