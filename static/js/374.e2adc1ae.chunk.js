1ae683d01a529b73b20f80d33613838b3b288585
rash010 <rashmi@sensarobotics.com>
2024-05-01 21:10:13 +0530

    Updates

---
+++ static/js/374.e2adc1ae.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkswiggy_app=self.webpackChunkswiggy_app||[]).push([[374],{809:(e,s,a)=>{a.r(s),a.d(s,{default:()=>n});a(791);var i=a(184);const n=()=>(0,i.jsxs)("div",{children:["Groceries",(0,i.jsx)("p",{children:"showing off lazy loading, by creating seperate bundle"})]})}}]);
+//# sourceMappingURL=374.e2adc1ae.chunk.js.map
